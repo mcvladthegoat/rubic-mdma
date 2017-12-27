481e2b7802fcf92633af7199418ece0aa049cc9a
Andrey THEGOD <zhuravandrey@gmail.com>
2017-12-27 05:47:34 +0300

    Revert "Merge branch 'master' of https://github.com/mcvladthegoat/rubic-mdma"
    This reverts commit 1f1c8667aa189b9d1831992100b7ed9a9fde6d13, reversing
    changes made to 602a6d5d1b47917a9d340982fb0c36a68c3fc0cc.

--- Form1.cs
+++ Form1.cs
@@ -84,39 +84,21 @@ namespace mdma
             newCol.Name = "Position " + candCount;
             newCol.Visible = true;
             newCol.Width = 100;
-            newCol.ValueType = typeof(int);
             dataGridView1.Columns.Add(newCol);
         }
 
         private void bordaMethod_Click(object sender, EventArgs e)
         {
             dataGridView2.Rows.Clear();
-            var cndList = new List<int>();
-            for (int i = 0; i < dataGridView1.Columns.Count - 1; i++)
+            foreach (DataGridViewRow row in dataGridView1.Rows)
             {
-                cndList.Add(0);
-            }
-            for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
-            {
-
-                for (int j = 1; j < dataGridView1.ColumnCount; j++)
+                dataGridView2.Rows.Add(dataGridView2.Rows.Add(new DataGridViewRow()));
+                for (int i = 1; i < dataGridView1.ColumnCount; i++)
                 {
-<<<<<<< HEAD
-                    cndList[j - 1] += Convert.ToInt32(dataGridView1.Rows[i].Cells[j].Value);
-=======
                     dataGridView1.Rows[i-1].Cells[0] = new DataGridViewTextBoxCell() {Value = "Position " + i};
                     dataGridView1.Rows[i - 1].Cells[0] = new DataGridViewTextBoxCell() { Value = row.Cells[i]};
->>>>>>> 602a6d5d1b47917a9d340982fb0c36a68c3fc0cc
                 }
             }
-
-            for (int i = 0; i < cndList.Count; i++)
-            {
-                dataGridView2.Rows.Add(1);
-                dataGridView2.Rows[i].Cells[0] = new DataGridViewTextBoxCell() { Value = "Candidate " + (i + 1) };
-                dataGridView2.Rows[i].Cells[1] = new DataGridViewTextBoxCell() { Value = cndList[i] };
-            }
-
         }
 
         private void compRulesMethod_Click(object sender, EventArgs e)
